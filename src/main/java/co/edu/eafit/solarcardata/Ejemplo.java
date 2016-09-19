a46fea562632a7582b0522fd839b89af2f7b061c
danielr9911 <dani-9911@hotmail.com>
2016-09-19 08:55:59 -0500

    Solucionado error de compilacion

--- src/main/java/co/edu/eafit/solarcardata/Ejemplo.java
+++ src/main/java/co/edu/eafit/solarcardata/Ejemplo.java
@@ -3,7 +3,9 @@ package co.edu.eafit.solarcardata;
 
 import java.util.concurrent.TimeUnit;
 import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDB.ConsistencyLevel;
 import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
 import org.influxdb.dto.Query;
 
@@ -19,29 +21,32 @@ import org.influxdb.dto.Query;
  */
 public class Ejemplo {
     public static void main(String[] args){
-        InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8083", "root", "root");
-String dbName = "aTimeSeries";
-influxDB.createDatabase(dbName);
+        InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+        String dbName = "aTimeSeries";
+        influxDB.createDatabase(dbName);
 
-// Flush every 2000 Points, at least every 100ms
-influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
-
-Point point1 = Point.measurement("cpu")
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .tag("async", "true")
+                .retentionPolicy("autogen")
+                .consistency(ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("cpu")
                     .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
                     .addField("idle", 90L)
                     .addField("user", 9L)
                     .addField("system", 1L)
                     .build();
-Point point2 = Point.measurement("disk")
+        Point point2 = Point.measurement("disk")
                     .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
                     .addField("used", 80L)
                     .addField("free", 1L)
                     .build();
-
-influxDB.write(dbName, "autogen", point1);
-influxDB.write(dbName, "autogen", point2);
-Query query = new Query("SELECT idle FROM cpu", dbName);
-influxDB.query(query);
-influxDB.deleteDatabase(dbName);
+        batchPoints.point(point1);
+        batchPoints.point(point2);
+        influxDB.write(batchPoints);
+        Query query = new Query("SELECT idle FROM cpu", dbName);
+        influxDB.query(query);
+        //influxDB.deleteDatabase(dbName);
     }
 }
