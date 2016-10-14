3506688644b3d9758743e7618fb6f1bdbf151950
lauramv21 <laura_mv21@hotmail.com>
2016-10-14 18:29:58 -0500

    Bateria

--- src/main/java/co/edu/eafit/solarcardata/Bateria.java
+++ src/main/java/co/edu/eafit/solarcardata/Bateria.java
@@ -11,9 +11,18 @@ package co.edu.eafit.solarcardata;
  */
 public class Bateria extends Dato{
     public double cmutmax;
+    public double socpercent;
+    public double cellvmin;
+    public double cellvmax;
+    public double celltmax;
     
-    public Bateria(double cmutmax ){
+    public Bateria(double cmutmax, double socpercent, double cellvmin,
+            double cellvmax, double celltmax){
         this.cmutmax=cmutmax;
+        this.socpercent = socpercent;
+        this.cellvmin = cellvmin;
+        this.cellvmax = cellvmax;
+        this.celltmax = celltmax;
         
     }
 }
