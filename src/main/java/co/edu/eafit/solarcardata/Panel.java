ee04d1138c7aa4bbf68d9a79c5528ac152aacf43
lauramv21 <laura_mv21@hotmail.com>
2016-10-14 19:43:08 -0500

    mppts out

--- src/main/java/co/edu/eafit/solarcardata/Panel.java
+++ src/main/java/co/edu/eafit/solarcardata/Panel.java
@@ -16,17 +16,36 @@ public class Panel extends Dato{
     public double mpptvin0;
     public double mpptvin1;
     public double mpptvin2;
-    
+    public double mpptiin0;
+    public double mpptiin1;
+    public double mpptiin2;
+    public double mpptvout0;
+    public double mpptvout1;
+    public double mpptvout2;
+    public double mpptiout0;
+    public double mpptiout1;
+    public double mpptiout2;
             
             
     public Panel(double mpptflagspanel0, double mpptflagspanel1, 
             double mpptflagspanel2, double mpptvin0, double mpptvin1,
-            double mpptvin2){
+            double mpptvin2, double mpptiin0, double mpptiin1, double mpptiin2,
+            double mpptvout0, double mpptvout1, double mpptvout2, 
+            double mpptiout0, double mpptiout1, double mpptiout2){
         this.mpptflagspanel0 = mpptflagspanel0;
         this.mpptflagspanel1 = mpptflagspanel1;
         this.mpptflagspanel2 = mpptflagspanel2;
         this.mpptvin0 = mpptvin0;
         this.mpptvin1 = mpptvin1;
         this.mpptvin2 = mpptvin2;
+        this.mpptiin0 = mpptiin0;
+        this.mpptiin1 = mpptiin1;
+        this.mpptiin2 = mpptiin2;
+        this.mpptvout0 = mpptvout0;
+        this.mpptvout1 = mpptvout1;
+        this.mpptvout2 = mpptvout2;
+        this.mpptiout0 = mpptiout0;
+        this.mpptiout1 = mpptiout1;
+        this.mpptiout2 = mpptiout2;
         
 }
